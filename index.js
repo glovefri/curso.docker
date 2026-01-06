63dda60c8597ac22be647bfd0b68ff1419081bba
glovefri <gloria.figueroa@comunidadfeliz.cl>
2026-01-06 13:39:48 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+const PORT = 8080
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+  console.log('Hello World!')
+})
+
+// adding a comment
